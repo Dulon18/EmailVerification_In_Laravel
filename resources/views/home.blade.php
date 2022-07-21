33c278a18a15810a2b0810d7e8c7eb43686b2fe9
Dulon18 <dulon587@gmail.com>
2022-07-21 12:21:00 +0600

    email verification

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                    <h3 class="text-center mt-3 fw-bold">Welcome To Home Page</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
